37a46fc8779088a4fb1ac8290eb4344ab6edf278
urs-santoshh <santosh18timalsina@gmail.com>
2024-09-17 16:50:26 +0545

    new dataset used

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # app.py
 import streamlit as st
 import pandas as pd
-import numpy as np
 import joblib
 
 # Load the trained model
@@ -9,10 +8,10 @@ model = joblib.load("./models/best_random_forest_model.pkl")
 
 # Define expected feature columns based on the training set
 expected_columns = [
-    "Age", "Biomass_Fuel_Exposure", "Occupational_Exposure", "Family_History_COPD",
+    "Age","Duration_Of_Smoking", "Biomass_Fuel_Exposure", "Occupational_Exposure", "Family_History_COPD",
     "BMI", "Air_Pollution_Level", "Respiratory_Infections_Childhood", "Pollution_Risk_Score",
-    "Smoking_Status_encoded", "Gender_", "Smoking_Pollution_Interaction",
-    "Location_Biratnagar", "Location_Butwal", "Location_Chitwan", "Location_Dharan",
+    "Smoking_Status_encoded", "Gender", "Smoking_Pollution_Interaction",
+    "Location_Butwal", "Location_Chitwan", "Location_Dharan",
     "Location_Hetauda", "Location_Kathmandu", "Location_Lalitpur", "Location_Nepalgunj",
     "Location_Pokhara"
 ]
@@ -22,6 +21,7 @@ st.write("Enter the input values to predict COPD Diagnosis")
 
 # Collect user input for each feature
 age = st.number_input("Age", min_value=0, max_value=120, value=30)
+duration_of_smoking = st.number_input("Duration_Of_Smoking", min_value=0, max_value=80, value=0)
 biomass_fuel_exposure = st.selectbox("Biomass Fuel Exposure", [0, 1])
 occupational_exposure = st.selectbox("Occupational Exposure", [0, 1])
 family_history_copd = st.selectbox("Family History of COPD", [0, 1])
@@ -34,20 +34,21 @@ pollution_risk_score = st.number_input(
     "Pollution Risk Score", min_value=0.0, max_value=100.0, value=0.0)
 smoking_status_encoded = st.slider(
     "Smoking Status (0 = Non-smoker, 1 = Smoker)", 0.0, 1.0, 0.5)
-gender_ = st.selectbox("Gender (0 = Male, 1 = Female)", [0, 1])
+gender = st.selectbox("Gender (0 = Male, 1 = Female)", [0, 1])
 smoking_pollution_interaction = st.number_input(
     "Smoking Pollution Interaction", min_value=0.0, max_value=500.0, value=0.0)
 
 # Categorical Location Input
 location = st.selectbox(
     "Location",
-    ["Biratnagar", "Butwal", "Chitwan", "Dharan", "Hetauda", "Kathmandu",
+    ["Butwal", "Chitwan", "Dharan", "Hetauda", "Kathmandu",
      "Lalitpur", "Nepalgunj", "Pokhara"]
 )
 
 # Create a DataFrame with the input values
 input_data = pd.DataFrame([{
     "Age": age,
+    "Duration_Of_Smoking": duration_of_smoking,
     "Biomass_Fuel_Exposure": biomass_fuel_exposure,
     "Occupational_Exposure": occupational_exposure,
     "Family_History_COPD": family_history_copd,
@@ -56,9 +57,8 @@ input_data = pd.DataFrame([{
     "Respiratory_Infections_Childhood": respiratory_infections_childhood,
     "Pollution_Risk_Score": pollution_risk_score,
     "Smoking_Status_encoded": smoking_status_encoded,
-    "Gender_": gender_,
+    "Gender": gender,
     "Smoking_Pollution_Interaction": smoking_pollution_interaction,
-    "Location_Biratnagar": location == "Biratnagar",
     "Location_Butwal": location == "Butwal",
     "Location_Chitwan": location == "Chitwan",
     "Location_Dharan": location == "Dharan",
